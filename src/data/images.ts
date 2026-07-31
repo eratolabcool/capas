ed8e2f0cfc9006c8f9c695cd6e55dd444fe49c5f
eratolabcool <eratolabcool@gmail.com>
2026-07-31 14:35:32 +0800

    feat: add centralized image asset map for CapasPro

---
+++ src/data/images.ts
@@ -0,0 +1,15 @@
+export const capasImages = {
+  products: {
+    sofa: '/images/products/sofa-cover.jpg',
+    table: '/images/products/table-cover.jpg',
+    industrial: '/images/products/industrial-cover.jpg',
+  },
+  projects: {
+    hotelPool: '/images/projects/hotel-pool.jpg',
+    residential: '/images/projects/residential-outdoor.jpg',
+  },
+  guides: {
+    measureSofa: '/images/guides/measure-sofa.jpg',
+    measureRoundTable: '/images/guides/measure-round-table.jpg',
+  },
+};
