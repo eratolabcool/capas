4447369d98c856fa16a6438f395a18ad21d073cf
eratolabcool <eratolabcool@gmail.com>
2026-07-31 21:46:09 +0800

    feat: enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://capas.pt',
-  output: 'static'
+  output: 'static',
+  integrations: [sitemap()],
 });
